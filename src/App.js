3c23949f466f4ada0181a55b61d20378a7efd107
Mario <marioolinto@gmail.com>
2023-09-05 23:23:55 -0400

    Taking turns and Declaring a winner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 
 function Square({ value, onSquareClick }) {
-
-  //const [value, setValue] = useState(null);
-
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -13,17 +10,37 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() { 
   
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   
   function handleClick(i){
-    console.log(i);
+    
+    if (squares[i]) {
+      return;
+    }
+    
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
-    setSquares(nextSquares);    
+
+    if (xIsNext) {
+      nextSquares[i] = "X"
+    } else {
+      nextSquares[i] = "0"  
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+  
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner " + winner;
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "0");
   }
 
   return (
     <React.Fragment>
+      <div className="status">{status}</div>
       <div className="board-row">        
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -41,4 +58,27 @@ export default function Board() {
       </div>
     </React.Fragment>     
   );
+}
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  
+  return null;
+
 }
